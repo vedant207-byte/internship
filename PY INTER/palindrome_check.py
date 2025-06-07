e98a0c4a914677583fcc9cefd5bed66d7384522c
vedant207-byte <vedantbhusari207@gmail.com>
2025-06-07 23:52:39 +0530

    Delete PY INTER directory

--- PY INTER/palindrome_check.py
+++
@@ -1,5 +0,0 @@
-txt = input("Enter a string:")
-
-is_palindrome = txt == txt[::-1]
-
-print("palindrome:",is_palindrome)
--- PY INTER/reverse_string.py
+++
@@ -1,5 +0,0 @@
-text = input("Enter a string:")
-
-rev_txt = text[::-1]
-
-print("reverse string is:",rev_txt)
